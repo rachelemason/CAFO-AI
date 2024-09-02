4a8dcba59b6845abac223fd1394cd1ea924dd9da
Rachel Mason <rachel.e.mason1@gmail.com>
2024-09-02 21:06:12 +0000

    Fixed bug in explore.join_farms_and_buildings, added <factor> option to stratified_sample, removed unused functions. Modified selection of final farm sample in Iowa.ipynb

--- explore.py
+++ explore.py
@@ -39,29 +39,6 @@ def histo(df, ax_a, ax_b, animal, quantity_kw):
     ax_b.set_ylabel(f'Cumulative number of {animal}')
 
 
-def farms_per_region(df, ax, num, location_kw):
-  """
-  Create a histogram showing the number of farms per <location_kw> (e.g.
-  Comuna, Municipio)
-  """
-
-  df = df.groupby([location_kw]).size().sort_values(ascending=False).reset_index(name="Num")
-  ax.bar(x=df[location_kw], height=df['Num'], color='w', edgecolor='0.5',
-                    width=1)
-  for n, comuna in enumerate(df[location_kw][:10]):
-    ax.text(0.98, 0.96-0.05*n, comuna, ha='right', va='center',\
-            fontsize=8, transform=ax.transAxes)
-  ax.axvline(num, color='blue', ls='--')
-  ax.set_xticks([])
-  ax.set_xlabel(f'{location_kw}')
-  ax.set_ylabel(f'Number of farms')
-
-  total = df['Num'].sum()
-  in_top_n = df['Num'].head(num).sum()
-  percent = in_top_n / total * 100
-  print(f'The top {num} {location_kw} account for {in_top_n}/{total} farms ({percent:.0f}%)')
-
-
 def join_farms_and_buildings(farms, buildings, farm_dist, not_farm_dist, crs):
   """
   Return a df containing the farms in <farms> and the buildings in <buildings>
@@ -86,11 +63,16 @@ def join_farms_and_buildings(farms, buildings, farm_dist, not_farm_dist, crs):
   # Inner join on buffered farms with buildings
   farm_buildings = buffered.sjoin(buildings, how='inner', predicate='intersects')
   # Make the building geometry the primary geometry, retain farm geom in a
-  # separate column
+  # separate column, rename the CRS column
   farm_buildings.loc[:, 'Parent coords'] = farm_buildings.loc[:, 'geometry']
   farm_buildings.loc[:, 'geometry'] = farm_buildings.loc[:, 'buildings_geom']
-  # Keep any of these cols, if present; drop a load of other columns which
-  # just make it hard to inspect the df
+  # This is just for datasets like Chile that need multiple CRSs
+  try:
+    farm_buildings.loc[:, 'CRS'] = farm_buildings.loc[:, 'CRS_left']
+  except KeyError:
+    pass
+  # Keep any of these cols, if present, because they are useful; drop a load of
+  # other columns which just make it hard to inspect the df
   cols_to_keep = ['geometry', 'Parent coords', 'Area (sq m)', 'Farm type',\
                   'Number of animals', 'Number of pigs', 'Number of poultry',\
                   'CAFO class', 'Animal units (pigs)', 'Animal units (poultry)',\
@@ -100,14 +82,16 @@ def join_farms_and_buildings(farms, buildings, farm_dist, not_farm_dist, crs):
 
   # Find the buildings more than <not_farm_dist> from farm coords
 
-  buffered = buffer_gdf(farms, not_farm_dist)
-  # Left join on buildings with buffered farms
-  joined = buildings.sjoin(buffered, how="left", predicate='intersects')
-  # ID rows with no matches, i.e. buildings outside farm buffer
-  # Don't bother removing duplicates, isn't worth it
-  not_farm_buildings = joined[joined['index_right'].isnull()]
-  # Tidy the gdf. For these buildings there is no farm/lagoon info to preserve
-  not_farm_buildings = not_farm_buildings.filter(['geometry', 'Area (sq m)'])
+  buildings.to_crs(crs, inplace=True)
+  farms.to_crs(crs, inplace=True)
+  near_farms = buildings.sjoin_nearest(farms, how='inner',\
+                                       max_distance=not_farm_dist)
+  not_farm_buildings = buildings[~buildings.index.isin(near_farms.index)].copy()
+  not_farm_buildings.to_crs("EPSG:4326", inplace=True)
+
+  # Tidy the gdf. For these buildings there is no farm info to preserve
+  not_farm_buildings = not_farm_buildings.filter(['geometry', 'Area (sq m)',\
+                                                  'CRS'])
 
   return farm_buildings, not_farm_buildings
 
@@ -147,86 +131,8 @@ def define_bins():
   return bins
 
 
-def show_n_save(df, bins, fig_name=None, df_name=None, title=''):
-  """
-  Plots histograms for a single dataset. Intended for use with the DMV and NC
-  building datasets
-  """
-
-  _, axes = plt.subplots(1, 3, figsize=(9, 3.5))
-
-  cols = ["Area (sq m)", 'Length (m)', 'Aspect ratio']
-  stats = {}
-  for ax, col, in zip(axes, cols):
-    ax.hist(df[col], bins=bins[col], color='k', histtype='step')
-    ax.set_xlabel(col)
-    ax.set_ylabel('Frequency')
-    stats[col] = (df[col].min(), df[col].median(), df[col].max())
-
-  plt.suptitle(title, fontsize=10)
-  plt.tight_layout()
-  if fig_name is not None:
-    plt.savefig(f'/content/drive/MyDrive/CAFO_data/Analysis/{fig_name}.png')
-
-  stats_df = pd.DataFrame(stats, index=["Min", "Med", "Max"])
-  display(stats_df)
-  if df_name is not None:
-    stats_df.to_pickle(f'/content/drive/MyDrive/CAFO_data/Analysis/{df_name}.pkl')
-
-
-def building_scatterplots(df, dist, animal_type, compare, fname, axes):
-
-  def axis_stuff(ax):
-    ax.set_xlim(100, 2e6)
-    ax.set_xscale('log')
-    ax.set_xlabel(f'Reported number of {animal_type.lower()}')
-
-  def plot_compare(animal_type, item, ax):
-    # Indicate DMV poultry or NC pigs stats
-    if animal_type == 'Poultry' and compare is not None:
-      ax.axhline(compare[item].median(), ls="--", lw=1, color='b')
-      ax.axhline(compare[item].min(), ls=":", lw=1, color='b')
-      ax.axhline(compare[item].max(), ls=":", lw=1, color='b')
-    elif animal_type == "Pig" and compare is not None:
-      ax.axhline(compare[item].median(), ls="--", lw=1, color='b')
-
-  farms = df.groupby('Parent coords')
-
-  # Max building size vs quantity of animals
-  for farm in farms:
-    n_animals = farm[1]['Number of animals'].unique()[0]
-    med_area = farm[1]['Area (sq m)']
-    axes[0].scatter(n_animals, med_area, marker='o', s=8, color='k')
-  plot_compare(animal_type, 'Area (sq m)', axes[0])
-  axis_stuff(axes[0])
-  axes[0].set_ylim(0, 7000)
-  axes[0].set_ylabel(f'Max area (sq m) of buildings within {dist} m')
-
-  # Max building length vs quantity of animals
-  for farm in farms:
-    n_animals = farm[1]['Number of animals'].unique()[0]
-    med_length = farm[1]['Length (m)'].max()
-    axes[1].scatter(n_animals, med_length, marker='o', s=8, color='k')
-  plot_compare(animal_type, 'Length (m)', axes[1])
-  axis_stuff(axes[1])
-  axes[1].set_ylim(0, 275)
-  axes[1].set_ylabel(f'Max length (m) of buildings within {dist} m')
-
-  # Max building aspect ratio vs quantity of animals
-  for farm in farms:
-    n_animals = farm[1]['Number of animals'].unique()[0]
-    med_aspect = farm[1]['Aspect ratio'].max()
-    axes[2].scatter(n_animals, med_aspect, marker='o', s=8, color='k')
-  plot_compare(animal_type, 'Aspect ratio', axes[2])
-  axis_stuff(axes[2])
-  axes[2].set_ylim(0, 18)
-  axes[2].set_ylabel(f'Max aspect ratio of buildings within {dist} m')
-
-  plt.tight_layout()
-  plt.savefig(f'/content/drive/MyDrive/CAFO_data/Analysis/{fname}_buildings.png')
-
-
-def stratified_sample(gdf_farm, gdf_nonfarm, property_name, bins):
+def stratified_sample(gdf_farm, gdf_nonfarm, property_name, bins,\
+                      factor=1):
   """
   Return a sample from <gdf_nonfarm> that matches <gdf_farm> in
   terms of the distribution of <property_name>.
@@ -247,9 +153,11 @@ def stratified_sample(gdf_farm, gdf_nonfarm, property_name, bins):
       bin_nonfarm = gdf_nonfarm[bin_mask]
 
       # Sample the same number of non-farm buildings as there are farm
-      # buildings in this bin, add to overall non-farm gdf
+      # buildings in this bin, multiplied by <factor>; add to overall non-farm
+      # gdf
       if len(bin_nonfarm) > 0:
-          sampled_bin_nonfarm = bin_nonfarm.sample(farm_hist[i],\
+          num = int(farm_hist[i] * factor)
+          sampled_bin_nonfarm = bin_nonfarm.sample(num,\
                                                    random_state=42,\
                                                    replace=True)
           sampled_nonfarm = pd.concat([sampled_nonfarm, sampled_bin_nonfarm])
